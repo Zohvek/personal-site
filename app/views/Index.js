cfa64c55e0d31eb869e5104fbad3b56f80245b6e
Ryan Hutton <hatnice42@gmail.com>
2019-11-28 13:27:32 -0600

    remove link to nix'd page

--- app/views/Index.js
+++ app/views/Index.js
@@ -16,7 +16,7 @@ const Index = () => (
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">my stats</Link>, {' '}
+        
         or <Link to="/contact">contact</Link> me.
       </p>
 
